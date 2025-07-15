d09a5b55dd72d0d099221aecb5096f9361df0131
Md-Anikul-islam-Binju <anikul.islam.binju@gmail.com>
2025-07-15 19:59:53 +0600

    up

--- resources/views/frontend/pages/userHome.blade.php
+++ resources/views/frontend/pages/userHome.blade.php
@@ -3,23 +3,23 @@
     <section class="banner-section2 section-bg py-120">
         <div class="container">
             <div class="banner-slider">
-{{--                @foreach($slider as $data)--}}
-{{--                    <div class="banner-item">--}}
-{{--                        <img src="{{asset('images/slider/'. $data->file )}}" alt="banner-img" class="w-100 img-fluid" />--}}
-{{--                    </div>--}}
-{{--                @endforeach--}}
-                <div class="banner-item">
-                    <img src="{{URL::TO('frontend/images/service/image-3.png')}}" alt="banner-img" class="w-100 img-fluid" />
-                </div>
-                <div class="banner-item">
-                    <img src="{{URL::TO('frontend/images/service/image-4.png')}}" alt="banner-img" class="w-100 img-fluid" />
-                </div>
-                <div class="banner-item">
-                    <img src="{{URL::TO('frontend/images/service/image-5.png')}}" alt="banner-img" class="w-100 img-fluid" />
-                </div>
-                <div class="banner-item">
-                    <img src="{{URL::TO('frontend/images/banner/banner.png')}}" alt="banner-img" class="w-100 img-fluid" />
-                </div>
+                @foreach($slider as $data)
+                    <div class="banner-item">
+                        <img src="{{asset('images/slider/'. $data->file )}}" alt="banner-img" class="w-100 img-fluid" />
+                    </div>
+                @endforeach
+{{--                <div class="banner-item">--}}
+{{--                    <img src="{{URL::TO('frontend/images/service/image-3.png')}}" alt="banner-img" class="w-100 img-fluid" />--}}
+{{--                </div>--}}
+{{--                <div class="banner-item">--}}
+{{--                    <img src="{{URL::TO('frontend/images/service/image-4.png')}}" alt="banner-img" class="w-100 img-fluid" />--}}
+{{--                </div>--}}
+{{--                <div class="banner-item">--}}
+{{--                    <img src="{{URL::TO('frontend/images/service/image-5.png')}}" alt="banner-img" class="w-100 img-fluid" />--}}
+{{--                </div>--}}
+{{--                <div class="banner-item">--}}
+{{--                    <img src="{{URL::TO('frontend/images/banner/banner.png')}}" alt="banner-img" class="w-100 img-fluid" />--}}
+{{--                </div>--}}
             </div>
             <div class="service-btn text-center">
                 <a href="{{route('user.order.decision')}}" class="theme-btn theme-btn-primary text-uppercase">Order Now <span class="ms-2"><i
